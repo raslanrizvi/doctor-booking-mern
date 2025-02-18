e65be97d62bd3955ed1e000473061338ec0a799c
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-02-18 19:47:04 +0530

    changed colors

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,29 +9,29 @@ const Navbar = () => {
   const [token, setToken] = useState(true)
 
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+    <div className='flex bg-primary items-center justify-between text-sm px-14 py-4 mb-5 border-b border-b-gray-400 rounded-b-[16px]'>
       <img
         onClick={() => navigate("/")}
         className='w-44 cursor-pointer'
         src={assets.logo}
         alt=''
       />
-      <ul className='hidden md:flex items-start gap-5 font-medium'>
+      <ul className='hidden md:flex items-start gap-5 text-white font-medium'>
         <NavLink to='/'>
           <li className='py-1'>HOME</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <hr className='border-none outline-none h-0.5 bg-secondary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/doctors'>
           <li className='py-1'>All Doctors</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <hr className='border-none outline-none h-0.5 bg-secondary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/about'>
           <li className='py-1'>About</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <hr className='border-none outline-none h-0.5 bg-secondary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/contact'>
           <li className='py-1'>Contact</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <hr className='border-none outline-none h-0.5 bg-secondary w-3/5 m-auto hidden' />
         </NavLink>
       </ul>
       <div className='flex items-center gap-4'>
@@ -66,7 +66,7 @@ const Navbar = () => {
         ) : (
           <button
             onClick={() => navigate("/login")}
-            className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'
+            className='bg-white text-primary px-8 py-3 rounded-full font-subHeading font-semibold hidden md:block hover:scale-[.98] transition-all duration-300'
           >
             Create Account
           </button>
