8972c335d9f33f48217e619c08b9169a139454a1
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-02-18 22:21:40 +0530

    Completed Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+import { assets } from "../assets/assets"
+
+const Footer = () => {
+  return (
+    <div className='bg-primary rounded-t-[16px] text-white font-text select-none'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 px-20 py-20 mt-48 text-sm'>
+        {/* ---- Left Section ---- */}
+        <div>
+          <img
+            draggable='false'
+            className='mb-5 w-40'
+            src={assets.logo}
+            alt=''
+          />
+          <p className='w-full md:w-2/3  leading-6'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+
+        {/* ---- Center Section ---- */}
+        <div>
+          <p className='text-lg font-heading font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2'>
+            <li>Home</li>
+            <li>About US</li>
+            <li>Contact Us</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+
+        {/* ---- Right Section ---- */}
+        <div className='text-wrap'>
+          <p className='text-lg font-heading font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2'>
+            <li>+94-11-576-6942</li>
+            <li>contact@medicey.lk</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        {/* ---- Copyright Tag ---- */}
+        <hr className='h-1 w-[80%] bg-[#05251b] border-0 mx-[auto] rounded-sm' />
+        <p className='py-5 text-sm text-center font-headingTwo font-semibold'>
+          Copyright © 2025 MediCey - All Right Reserved.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
