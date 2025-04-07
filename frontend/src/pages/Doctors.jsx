9fd6d9ebb50d9e63df73b512e2c5638edeb48bea
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-07 15:59:56 +0530

    Completed Doctores Page

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -23,15 +23,87 @@ const Doctors = () => {
 
   return (
     <div>
-      <p className='text-grey-600'>Browse through the doctors specialist.</p>
-      <div>
-        <div>
-          <p>General physician</p>
-          <p>Gynecologist</p>
-          <p>Dermatologist</p>
-          <p>Pediatricians</p>
-          <p>Neurologist</p>
-          <p>Gastroenterologist</p>
+      <p className='text-grey-600 text-xl'>
+        Browse through the doctors specialist.
+      </p>
+      <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+        <div className='flex flex-col gap-4 text-sm yext-gray-600'>
+          <p
+            onClick={() =>
+              speciality === "General Physician"
+                ? navigate("/doctors")
+                : navigate("/doctors/General Physician")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-[#047e3b]/20 rounded-lg transition-all cursor-pointer text-grey-600 text-base font-normal ${
+              speciality === "General Physician"
+                ? "bg-[#e7f8ea] text-grey-700"
+                : ""
+            }`}
+          >
+            General Physician
+          </p>
+          <p
+            onClick={() =>
+              speciality === "Gynecologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Gynecologist")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-[#047e3b]/20 rounded-lg transition-all cursor-pointer text-grey-600 text-base font-normal ${
+              speciality === "Gynecologist" ? "bg-[#e7f8ea] text-grey-700" : ""
+            }`}
+          >
+            Gynecologist
+          </p>
+          <p
+            onClick={() =>
+              speciality === "Dermatologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Dermatologist")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-[#047e3b]/20 rounded-lg transition-all cursor-pointer text-grey-600 text-base font-normal ${
+              speciality === "Dermatologist" ? "bg-[#e7f8ea] text-grey-700" : ""
+            }`}
+          >
+            Dermatologist
+          </p>
+          <p
+            onClick={() =>
+              speciality === "Pediatricians"
+                ? navigate("/doctors")
+                : navigate("/doctors/Pediatricians")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-[#047e3b]/20 rounded-lg transition-all cursor-pointer text-grey-600 text-base font-normal ${
+              speciality === "Pediatricians" ? "bg-[#e7f8ea] text-grey-700" : ""
+            }`}
+          >
+            Pediatricians
+          </p>
+          <p
+            onClick={() =>
+              speciality === "Neurologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Neurologist")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-[#047e3b]/20 rounded-lg transition-all cursor-pointer text-grey-600 text-base font-normal ${
+              speciality === "Neurologist" ? "bg-[#e7f8ea] text-grey-700" : ""
+            }`}
+          >
+            Neurologist
+          </p>
+          <p
+            onClick={() =>
+              speciality === "Gastroenterologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Gastroenterologist")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-[#047e3b]/20 rounded-lg transition-all cursor-pointer text-grey-600 text-base font-normal ${
+              speciality === "Gastroenterologist"
+                ? "bg-[#e7f8ea] text-grey-700"
+                : ""
+            }`}
+          >
+            Gastroenterologist
+          </p>
         </div>
         <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
           {filterDoc.map((item, index) => (
