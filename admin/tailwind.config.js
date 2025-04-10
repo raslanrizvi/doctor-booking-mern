253f6a72beca9895eb23a361d206c40c4729f0b3
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-11 02:20:12 +0530

    Added Custom Colors

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3D9E4F",
+        navFot: "#047E3B",
+        secondary: "#4a9e5a",
+        cardBg: "#E0F1E7",
+      },
+    },
   },
   plugins: [],
 }
