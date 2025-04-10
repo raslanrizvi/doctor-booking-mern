1c583d005e0a1ca1d8f84ebc461f7408d3412779
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-11 02:18:45 +0530

    Fixes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react"
-
+import Login from "./pages/Login.jsx"
 const App = () => {
   return (
-    <div className='text-green-700'>
+    <div>
       <Login />
     </div>
   )
