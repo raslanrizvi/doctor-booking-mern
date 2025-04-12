953f18de7121cafd25a4658c8023f04ffc6e8300
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-13 04:19:49 +0530

    Created getProfile Function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,4 +81,17 @@ const loginUser = async (req, res) => {
   }
 }
 
-export { registerUser, loginUser }
+//API to get user profile data
+const getProfile = async (req, res) => {
+  try {
+    const userId = req.userId
+    const userData = await userModel.findById(userId).select("-password")
+
+    res.json({ success: true, userData })
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: "getProfile" + error.message })
+  }
+}
+
+export { registerUser, loginUser, getProfile }
