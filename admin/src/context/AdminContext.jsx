a69b9dbb342e5d22ad1c9184bba96e9435a69897
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-12 07:16:08 +0530

    Fixes

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,9 +1,21 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
-  const value = {}
+  //state to remember aToken
+  const [aToken, setAToken] = useState(
+    //useState checks for aToken in Local Storage
+    localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
+  )
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+  const value = {
+    aToken,
+    setAToken,
+    backendUrl,
+  }
   return (
     <AdminContext.Provider value={value}>
       {props.children}
