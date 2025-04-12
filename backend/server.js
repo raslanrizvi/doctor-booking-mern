f2ae2086efd804789cc2fa6e8b3d3f4fcb96f0ae
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-12 23:50:44 +0530

    Created API for user

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
 import adminRouter from "./routes/adminRoute.js"
 import doctorRouter from "./routes/doctorRoute.js"
+import userRouter from "./routes/userRoutes.js"
 
 //app config
 const app = express()
@@ -17,10 +18,10 @@ app.use(express.json())
 app.use(cors())
 
 //api endpoints
-app.use("/api/admin", adminRouter)
 // localhost:4000/api/admin/add-doctor
-
+app.use("/api/admin", adminRouter)
 app.use("/api/doctor", doctorRouter)
+app.use("/api/user", userRouter)
 
 app.get("/", (req, res) => {
   res.send("API Working Great")
