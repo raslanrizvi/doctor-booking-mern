81988032cf68805112f020fed402ba2b1e47ea0c
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-13 01:35:24 +0530

    userLogin Function created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -56,4 +56,29 @@ const registerUser = async (req, res) => {
   }
 }
 
-export { registerUser }
+//API for User Login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await userModel.findOne({ email })
+
+    if (!user) {
+      return res.json({ success: false, message: "User Does Not Exist" })
+    }
+
+    //checking if password is matching
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+      res.json({ success: true, token })
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" })
+    }
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
+  }
+}
+
+export { registerUser, loginUser }
