b3fa399f4aa0438caae744ba77f0c2600905db8d
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-14 02:25:17 +0530

    Added update-Profile Route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,24 @@ import {
   getProfile,
   loginUser,
   registerUser,
+  updateProfile,
 } from "../controllers/userController.js"
 import authUser from "../middlewares/authUser.js"
+import upload from "../middlewares/multer.js"
 
 const userRouter = express.Router()
 
+//Authentication Route
 userRouter.post("/register", registerUser)
 userRouter.post("/login", loginUser)
+
+//profile route
 userRouter.get("/get-profile", authUser, getProfile)
+userRouter.post(
+  "/update-profile",
+  upload.single("image"),
+  authUser,
+  updateProfile
+)
 
 export default userRouter
