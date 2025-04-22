d018c7a54ad7b0ffb095d9dd6ee407f32f2e2156
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-23 04:23:36 +0530

    Added route for stripe

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,7 @@ import {
   bookAppointment,
   listAppointment,
   cancelAppointment,
+  paymentStripe,
 } from "../controllers/userController.js"
 import authUser from "../middlewares/authUser.js"
 import upload from "../middlewares/multer.js"
@@ -28,5 +29,6 @@ userRouter.post(
 userRouter.post("/book-appointment", authUser, bookAppointment)
 userRouter.get("/appointments", authUser, listAppointment)
 userRouter.post("/cancel-appointment", authUser, cancelAppointment)
+userRouter.post("/payment-stripe", authUser, paymentStripe)
 
 export default userRouter
