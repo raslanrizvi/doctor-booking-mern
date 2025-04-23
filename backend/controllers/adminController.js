63ee2bc2545263e99315683d5bc0dd12c96f51e0
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-23 19:53:04 +0530

    Cancel Appointment Admin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -131,4 +131,42 @@ const appointmentsAdmin = async (req, res) => {
   }
 }
 
-export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin }
+// API for Appointment Cancellation via Admin Dashboard
+const appointmentCancelAdmin = async (req, res) => {
+  try {
+    const { appointmentId } = req.body
+
+    const appointmentData = await appointmentModel.findById(appointmentId)
+
+    await appointmentModel.findByIdAndUpdate(appointmentId, { cancelled: true })
+
+    //releasing docSlot
+    const { docId, slotDate, slotTime } = appointmentData
+
+    const doctorData = await doctorModel.findById(docId)
+
+    let slots_booked = doctorData.slots_booked
+
+    slots_booked[slotDate] = slots_booked[slotDate].filter(
+      (e) => e !== slotTime
+    )
+
+    await doctorModel.findByIdAndUpdate(docId, { slots_booked })
+
+    res.json({ success: true, message: "Appointment Cancelled Successfully" })
+  } catch (error) {
+    console.log("Appointment Cancellation Failed" + error)
+    res.json({
+      success: false,
+      message: "Appointment Cancellation Failed" + error,
+    })
+  }
+}
+
+export {
+  addDoctor,
+  loginAdmin,
+  allDoctors,
+  appointmentsAdmin,
+  appointmentCancelAdmin,
+}
