8d9f8ea51edf008b4b73f1391c89e3198e85ace8
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-24 06:36:26 +0530

    sidebar for Doctor Created

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -3,9 +3,11 @@ import { useContext } from "react"
 import { AdminContext } from "../context/AdminContext"
 import { NavLink } from "react-router-dom"
 import { assets } from "../assets/assets"
+import { DoctorContext } from "../context/DoctorContext"
 
 const Sidebar = () => {
   const { aToken } = useContext(AdminContext)
+  const { dToken } = useContext(DoctorContext)
 
   return (
     <div className='min-h-screen bg-white border-r'>
@@ -54,6 +56,43 @@ const Sidebar = () => {
           >
             <img src={assets.people_icon} alt='' />
             <p>Doctors List</p>
+          </NavLink>
+        </ul>
+      )}
+      {dToken && (
+        <ul className='text-[#515151] font-medium mt-5'>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer rounded-r-xl ${
+                isActive ? "bg-[#e7f8ea] border-r-[5px] border-primary " : ""
+              }`
+            }
+            to={"/doctor-dashboard"}
+          >
+            <img src={assets.home_icon} alt='' />
+            <p>Dashboard</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer rounded-r-xl ${
+                isActive ? "bg-[#e7f8ea] border-r-[5px] border-primary " : ""
+              }`
+            }
+            to={"/doctor-appointments"}
+          >
+            <img src={assets.appointment_icon} alt='' />
+            <p>Appointments</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer rounded-r-xl ${
+                isActive ? "bg-[#e7f8ea] border-r-[5px] border-primary " : ""
+              }`
+            }
+            to={"/doctor-profile"}
+          >
+            <img src={assets.people_icon} alt='' />
+            <p>Profile</p>
           </NavLink>
         </ul>
       )}
