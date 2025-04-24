977c8f17d3aa82a41a672c4f4470b8b6496a4472
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-24 06:35:43 +0530

    Routes for Doctor Added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,6 +10,9 @@ import AllAppointments from "./pages/Admin/AllAppointments.jsx"
 import AddDoctor from "./pages/Admin/AddDoctor.jsx"
 import DoctorsList from "./pages/Admin/DoctorsList.jsx"
 import { DoctorContext } from "./context/DoctorContext.jsx"
+import DoctorDashboard from "./pages/Doctor/DoctorDashboard.jsx"
+import DoctorAppointment from "./pages/Doctor/DoctorAppointment.jsx"
+import DoctorProfile from "./pages/Doctor/DoctorProfile.jsx"
 
 const App = () => {
   //get aToken
@@ -23,11 +26,17 @@ const App = () => {
       <div className='flex items-start'>
         <Sidebar />
         <Routes>
+          {/* Admin Routes */}
           <Route path='/' element={<></>} />
           <Route path='/admin-dashboard' element={<Dashboard />} />
           <Route path='/all-appointments' element={<AllAppointments />} />
           <Route path='/add-doctor' element={<AddDoctor />} />
           <Route path='/doctor-list' element={<DoctorsList />} />
+
+          {/* Doctor Routes */}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppointment />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
         </Routes>
       </div>
     </div>
