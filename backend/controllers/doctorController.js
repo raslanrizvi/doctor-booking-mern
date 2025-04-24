b2ece3e5ad42540e09dad448e7cbea02c9eea25d
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-24 09:27:05 +0530

    AppointmentsDoctor function  Completed

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,6 +1,7 @@
 import doctorModel from "../models/doctorModel.js"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
+import appointmentModel from "../models/appoitmentModel.js"
 
 const changeAvailability = async (req, res) => {
   try {
@@ -51,4 +52,16 @@ const loginDoctor = async (req, res) => {
   }
 }
 
-export { changeAvailability, doctorList, loginDoctor }
+//API to get Appointments to Doctor Dashboard
+const appointmentsDoctor = async (req, res) => {
+  try {
+    const docId = req.docId
+    const appointments = await appointmentModel.find({ docId })
+    res.json({ success: true, appointments, docId })
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
+  }
+}
+
+export { changeAvailability, doctorList, loginDoctor, appointmentsDoctor }
