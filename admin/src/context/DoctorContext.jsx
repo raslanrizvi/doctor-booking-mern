153c3702276e97b5462d52f0e536832e26da1c7d
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-04-24 09:23:35 +0530

    getAppointments function completed

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useState } from "react"
+import axios from "axios"
+import { toast } from "react-toastify"
 
 export const DoctorContext = createContext()
 
@@ -9,11 +11,33 @@ const DoctorContextProvider = (props) => {
     //useState checks for dToken in Local Storage
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
   )
+  const [appointments, setAppointments] = useState([])
+
+  const getAppointments = async () => {
+    try {
+      const { data } = await axios.get(
+        backendUrl + "/api/doctor/appointments",
+        { headers: { dToken } }
+      )
+      if (data.success) {
+        setAppointments(data.appointments.reverse())
+        console.log(data.appointments.reverse())
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
 
   const value = {
     dToken,
     setDToken,
     backendUrl,
+    appointments,
+    setAppointments,
+    getAppointments,
   }
   return (
     <DoctorContext.Provider value={value}>
